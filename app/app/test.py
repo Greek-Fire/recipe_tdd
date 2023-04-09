01dba40020860be6c339d98d87ad464c66ef81a9
bob <57952350+TheRedGreek@users.noreply.github.com>
2023-04-08 22:11:19 -0400

    Update test.py

--- app/app/test.py
+++ app/app/test.py
@@ -12,7 +12,6 @@ class CalcTests(SimpleTestCase):
         """Test that two numbers are added together"""
         self.assertEqual(calc.add(3, 8), 11)
 
-
     def test_subtract_numbers(self):
         """Test that values are subtracted and returned"""
         self.assertEqual(calc.subtract(5, 11), 6)
